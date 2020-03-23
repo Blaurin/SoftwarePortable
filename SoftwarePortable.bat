03830e938d8850222838be4097250d651932a118
albreleo <54851179+albreleo@users.noreply.github.com>
2020-03-23 13:33:28 +0100

    Added the main Software-Engine

---
+++ SoftwarePortable.bat
@@ -0,0 +1,11 @@
+@echo off
+echo "    SoftwarePortable Version 1.1 Testing"
+echo "------------------------------------------------"
+echo "  Copyright albreleo and Blaurin on GitHub 2020 "
+echo "Options:"
+echo "Suitable for: Lightweight Software"
+echo "1: Copy the chosen Software to an external Device" 
+echo "2: Software aus Ordner auf externes Medium verschieben" 
+echo "3:30MB" 
+choice /c 123 /m "Bitte waehlen Sie eine Option aus:"
+pause
